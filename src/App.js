b46a7a82536b735650c321231504a417c289598e
Vinay Singh <vinaysthakur15@gmail.com>
2024-12-22 12:50:00 +0530

    Created Project with Component structure and App Routes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Router, Route, Routes } from "react-router-dom";
+import SignIn from "./Components/SignIn";
+import Home from "./Components/Home";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
