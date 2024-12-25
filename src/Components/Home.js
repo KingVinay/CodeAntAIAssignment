f40eb6cda37e23df529bb48ef9a025b9fe4c6d5d
Vinay Singh <vinaysthakur15@gmail.com>
2024-12-25 14:06:02 +0530

    Home Page Buttons and HAmburger UI fixed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import SmallLogo from "../Assets/SmallLogo.png";
-
+import down from "../Assets/down.png";
+import house from "../Assets/house.png";
+import code from "../Assets/code.png";
+import cloud from "../Assets/cloud.png";
+import book from "../Assets/book.png";
+import setting from "../Assets/setting.png";
+import support from "../Assets/support.png";
+import logout from "../Assets/logout.png";
+import refresh from "../Assets/refresh.png";
+import plus from "../Assets/plus.png";
+import search from "../Assets/search.png";
+import data from "../Assets/data.png";
 const Home = () => {
   const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
   const [selectedRepo, setSelectedRepo] = useState(null);
+  const navigate = useNavigate();
 
   const repositories = [
     {
@@ -58,7 +71,7 @@ const Home = () => {
   ];
 
   const handleLogout = () => {
-    // Handle logout logic, possibly redirect to Sign-In component
+    navigate("/");
   };
 
   const handleSelect = (repo) => {
@@ -68,124 +81,99 @@ const Home = () => {
   return (
     <div className="flex flex-col md:flex-row min-h-screen">
       {/* Left Component */}
-      <div
-        className={`w-full md:w-1/5 bg-gray-100 p-4 md:p-6 flex flex-col items-center md:block overflow-y-hidden top-0 z-10 ${
-          isHamburgerOpen ? "block" : "hidden"
-        }`}
-      >
-        <div className="flex text-center mb-6">
-          <img src={SmallLogo} alt="Logo" className="mb-4" />
-          <h2 className="text-2xl mt-2 ml-2 font-light">CodeAnt AI</h2>
+      <div className="flex flex-col items-left">
+        <div className="flex text-center mt-8 md:mt-4 ml-6">
+          <img src={SmallLogo} alt="Logo" className="w-7 h-8 mb-4" />
+          <h2 className="text-xl mt-2 ml-2 font-light">CodeAnt AI</h2>
         </div>
+        <div
+          className={`bg-white p-4 md:p-6 flex flex-col items-center md:block overflow-y-hidden top-20 ${
+            isHamburgerOpen ? "block absolute z-10 inset-0" : "hidden"
+          }`}
+        >
+          {/* Name Box */}
+          <div className="w-full h-9 mb-4 flex items-center justify-between border-[1px] border-custom-gray rounded-md p-2">
+            <span className="truncate max-w-[180px]">Vinay Singh</span>
+            <img src={down} alt="down arrow" className="cursor-pointer" />
+          </div>
 
-        {/* Name Box */}
-        <div className="w-full mb-6 flex items-center justify-between border border-gray-300 rounded-md p-2">
-          <span className="truncate max-w-[150px]">
-            {selectedRepo ? selectedRepo.name : "Select a repository"}
-          </span>
-          <img
-            src="down-arrow.svg"
-            alt="down arrow"
-            className="cursor-pointer"
-          />
-        </div>
+          {/* Buttons */}
+          <div className="w-full flex flex-col items-start space-y-2 md:mb-6">
+            <button className="flex items-center space-x-2 w-full h-9 text-white py-2 px-4 text-left bg-custom-blue border-[1px] border-custom-gray rounded-md">
+              <img src={house} alt="Repositories" className="w-5 h-5" />
+              <span className="text-sm">Repositories</span>
+            </button>
+            <button className="flex items-center space-x-2 w-full h-9 py-2 px-4 text-left bg-white rounded-md">
+              <img src={code} alt="AI Code Review" className="w-5 h-5" />
+              <span className="text-sm">AI Code Review</span>
+            </button>
+            <button className="flex items-center space-x-2 w-full h-9 py-2 px-4 text-left bg-white rounded-md">
+              <img src={cloud} alt="Cloud Security" className="w-5 h-5" />
+              <span className="text-sm">Cloud Security</span>
+            </button>
+            <button className="flex items-center space-x-2 w-full h-9 py-2 px-4 text-left bg-white rounded-md">
+              <img src={book} alt="How to Use" className="w-5 h-5" />
+              <span className="text-sm">How to Use</span>
+            </button>
+            <button className="flex items-center space-x-2 w-full h-9 py-2 px-4 text-left bg-white rounded-md">
+              <img src={setting} alt="Settings" className="w-5 h-5" />
+              <span className="text-sm">Settings</span>
+            </button>
+          </div>
 
-        {/* Buttons */}
-        <div className="w-full flex flex-col items-start space-y-2 mb-6">
-          <button className="flex items-center space-x-2 w-full py-2 px-4 text-left bg-white border rounded-md shadow-md hover:bg-gray-100">
-            <img
-              src="repositories-icon.svg"
-              alt="Repositories"
-              className="w-5 h-5"
-            />
-            <span>Repositories</span>
-          </button>
-          <button className="flex items-center space-x-2 w-full py-2 px-4 text-left bg-white border rounded-md shadow-md hover:bg-gray-100">
-            <img
-              src="ai-code-review-icon.svg"
-              alt="AI Code Review"
-              className="w-5 h-5"
-            />
-            <span>AI Code Review</span>
-          </button>
-          <button className="flex items-center space-x-2 w-full py-2 px-4 text-left bg-white border rounded-md shadow-md hover:bg-gray-100">
-            <img
-              src="cloud-security-icon.svg"
-              alt="Cloud Security"
-              className="w-5 h-5"
-            />
-            <span>Cloud Security</span>
-          </button>
-          <button className="flex items-center space-x-2 w-full py-2 px-4 text-left bg-white border rounded-md shadow-md hover:bg-gray-100">
-            <img
-              src="how-to-use-icon.svg"
-              alt="How to Use"
-              className="w-5 h-5"
-            />
-            <span>How to Use</span>
-          </button>
-          <button className="flex items-center space-x-2 w-full py-2 px-4 text-left bg-white border rounded-md shadow-md hover:bg-gray-100">
-            <img src="settings-icon.svg" alt="Settings" className="w-5 h-5" />
-            <span>Settings</span>
-          </button>
-        </div>
-
-        {/* Bottom Buttons */}
-        <div className="mt-20 w-full flex flex-col items-start space-y-2">
-          <button
-            onClick={handleLogout}
-            className="flex items-center space-x-2 w-full py-2 px-4 text-left bg-white border rounded-md shadow-md hover:bg-gray-100"
-          >
-            <img src="logout-icon.svg" alt="Logout" className="w-5 h-5" />
-            <span>Logout</span>
-          </button>
-          <button className="flex items-center space-x-2 w-full py-2 px-4 text-left bg-white border rounded-md shadow-md hover:bg-gray-100">
-            <img src="support-icon.svg" alt="Support" className="w-5 h-5" />
-            <span>Support</span>
-          </button>
+          {/* Bottom Buttons */}
+          <div className="md:mt-48 w-full flex flex-col items-start space-y-2">
+            <button className="flex items-center space-x-2 w-full h-9 py-2 px-4 text-left bg-white rounded-md">
+              <img src={support} alt="Support" className="w-5 h-5" />
+              <span className="text-sm">Support</span>
+            </button>
+            <button
+              onClick={handleLogout}
+              className="flex items-center space-x-2 w-full h-9 py-2 px-4 text-left bg-white rounded-md "
+            >
+              <img src={logout} alt="Logout" className="w-5 h-5" />
+              <span className="text-sm">Logout</span>
+            </button>
+          </div>
         </div>
       </div>
 
       {/* Right Component */}
       <div
-        className={`w-full md:w-4/5 bg-white p-4 md:p-6 ${
-          isHamburgerOpen ? "w-full" : "md:w-4/5"
+        className={`w-full md:w-4/5 bg-white p-4 md:p-6 relative ${
+          isHamburgerOpen ? "w-full bg-gray-600 opacity-50" : "md:w-4/5"
         }`}
       >
         <div className="flex flex-col md:justify-between md:flex-row">
           <div>
-            <div className="flex justify-between items-center mb-4">
-              <h3 className="text-2xl font-bold">Repositories</h3>
+            <div className="flex justify-between items-center mb-1">
+              <h3 className="text-xl font-bold">Repositories</h3>
             </div>
 
-            <span className="text-sm text-gray-500 mb-4">
+            <span className="text-sm text-gray-500 mb-2">
               50 total Repositories
             </span>
           </div>
 
           {/* For Mobile: Refresh & Add Repository above search */}
           <div className="flex space-x-2">
-            <button className="flex items-center md:h-10 md:w-40 py-2 px-4 bg-white text-black border-[1px] border-custom-gray rounded-md">
-              <img src="refresh-icon.svg" alt="Refresh" className="w-5 h-5" />
-              <span>Refresh</span>
+            <button className="flex items-center md:h-9 md:w-38 py-2 px-4 bg-white text-black border-[1px] border-custom-gray rounded-md">
+              <img src={refresh} alt="Refresh" className="w-4 h-4 mr-2" />
+              <span>Refresh All</span>
             </button>
-            <button className="flex items-center md:h-10 md:w-48 py-2 px-4 bg-blue-500 text-white rounded-md">
-              <img
-                src="add-repository-icon.svg"
-                alt="Add Repository"
-                className="w-5 h-5"
-              />
+            <button className="flex items-center md:h-9 md:w-44 py-2 px-4 bg-blue-500 text-white rounded-md">
+              <img src={plus} alt="Add Repository" className="w-4 h-4 mr-2" />
               <span>Add Repository</span>
             </button>
           </div>
         </div>
 
         {/* Search Input */}
-        <div className="mb-6 flex items-center space-x-2">
-          <img src="search-icon.svg" alt="Search" className="w-5 h-5" />
+        <div className="mt-4 mb-6 flex items-center space-x-2 md:w-80 px-4 w-full border-[1px] border-custom-gray">
+          <img src={search} alt="Search" className="w-5 h-5" />
           <input
             type="text"
-            className="p-2 border rounded-md w-full"
+            className="p-2 rounded-md"
             placeholder="Search repositories..."
           />
         </div>
